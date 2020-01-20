e5ec6db2c4b0e33f2b24e5f74e0b433bf3fc02c6
Jens Lövgren <jens.lovgren@outlook.com>
2020-01-20 13:23:33 +0100

    Lagt till RegisterVM-klass med proppar för registering

---
+++ FungiFinder/FungiFinder/Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FungiFinder.Models.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [Display(Name = "Användarnamn")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "E-Mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Lösenord")]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Lösenordsvalidering")]
+        [Compare("Password", ErrorMessage = "Lösenorden matchar inte")]
+        public string ConfirmPassword { get; set; }
+    }
+}
