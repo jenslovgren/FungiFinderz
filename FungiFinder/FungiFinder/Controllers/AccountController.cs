94db4ec658efcbe1892748cb6269012638ef2ef2
Eric <eric.runer@gmail.com>
2020-01-20 13:16:19 +0100

    Added proppar to LoginVM

--- FungiFinder/FungiFinder/Controllers/AccountController.cs
+++ FungiFinder/FungiFinder/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FungiFinder.Controllers
 {
+
     public class AccountController : Controller
     {
         [Route("")]
--- FungiFinder/FungiFinder/Models/ViewModels/LoginVM.cs
+++ FungiFinder/FungiFinder/Models/ViewModels/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,12 @@ namespace FungiFinder.Models.ViewModels
 {
     public class LoginVM
     {
-        
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
     }
 }
