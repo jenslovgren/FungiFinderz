29e1010002fc8fe2087058e4a4e84e0e8e7d135c
PontusNelander <pontus.nelander@live.se>
2020-01-20 15:56:59 +0100

    Made some database structure and changed the layout page

--- FungiFinder/FungiFinder/Models/MyIdentityUser.cs
+++ FungiFinder/FungiFinder/Models/MyIdentityUser.cs
@@ -9,6 +9,10 @@ namespace FungiFinder.Models
 {
     public class MyIdentityUser : IdentityUser
     {
-        
+        public string FavoriteMushroom { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public int MushroomId { get; set; }
+
+
     }
 }
