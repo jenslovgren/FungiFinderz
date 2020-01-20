70aa056466ad092433ccef555930439fd3fd8f39
Marie Degerström <marie@degerstrom.se>
2020-01-20 12:01:48 +0100

    Fixed db configuration

--- FungiFinder/FungiFinder/Startup.cs
+++ FungiFinder/FungiFinder/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,6 +16,12 @@ namespace FungiFinder
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,6 +36,9 @@ namespace FungiFinder
                 .AddEntityFrameworkStores<MyIdentityContext>()
                 .AddDefaultTokenProviders();
             services.AddTransient<AccountService>();
+
+            var constring = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MyIdentityContext>(o => o.UseSqlServer(constring));
 
 
             services.ConfigureApplicationCookie(o => o.LoginPath = "/login");
