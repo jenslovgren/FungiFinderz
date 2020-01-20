383f2708ed136754e9900852a28c3b6ea2159eba
PontusNelander <pontus.nelander@live.se>
2020-01-20 16:10:43 +0100

    Created a db Project

---
+++ FungiFinder/FungiFinder.Database/dbo/Tables/LatestSearches.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LatestSearches] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [SearchDate] DATETIME       NOT NULL,
+    [UserId]     NVARCHAR (450) NOT NULL,
+    [Mushroom]   NCHAR (64)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
---
+++ FungiFinder/FungiFinder.Database/dbo/Tables/Mushrooms.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Mushrooms] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [LatinName] NVARCHAR (64)  NOT NULL,
+    [Info]      NVARCHAR (450) NOT NULL,
+    [ImageUrl]  NVARCHAR (450) NOT NULL,
+    [Edible]    BIT            NOT NULL,
+    [Name]      NVARCHAR (64)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
