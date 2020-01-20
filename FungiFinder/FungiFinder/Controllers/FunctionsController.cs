07aac93ef8e47d09d109369230f9e743d1dab33a
PontusNelander <pontus.nelander@live.se>
2020-01-20 17:27:11 +0100

    Added Actions to the FunctionsController

--- FungiFinder/FungiFinder/Controllers/FunctionsController.cs
+++ FungiFinder/FungiFinder/Controllers/FunctionsController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FungiFinder.Controllers
 {
+    [Authorize]
     public class FunctionsController : Controller
     {
-        public IActionResult Index()
+        [Route("main")]
+        public IActionResult Main()
         {
             return View();
         }
+
+        [Route("library")]
+        public IActionResult Library()
+        {
+            return View();
+        }
+
+        [Route("profile")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        
     }
 }
