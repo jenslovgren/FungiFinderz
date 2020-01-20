6861fa645887e5c5c5c17566ad3c116544f4b4e9
axelfriberg01 <axel.friberg.work@gmail.com>
2020-01-20 14:30:22 +0100

    Added FunctionProfileVM with properties

---
+++ FungiFinder/FungiFinder/Models/ViewModels/FunctionProfileVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FungiFinder.Models.ViewModels
+{
+    public class FunctionProfileVM
+    {
+        public IFormFile ProfilePicture { get; set; }
+        public string Username { get; set; }
+        public string MushroomLookAlike { get; set; }
+        public string FavouriteMushroom { get; set; }
+        public string PreviousSearches { get; set; }
+
+
+    }
+}
