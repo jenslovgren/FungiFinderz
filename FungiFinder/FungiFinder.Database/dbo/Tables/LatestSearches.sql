0113061c707f70b9f69cf76e6d4959cf2b5450ac
PontusNelander <pontus.nelander@live.se>
2020-01-21 09:11:00 +0100

    Changed the datatype of Mushroom in table latestsearches

--- FungiFinder/FungiFinder.Database/dbo/Tables/LatestSearches.sql
+++ FungiFinder/FungiFinder.Database/dbo/Tables/LatestSearches.sql
@@ -2,7 +2,7 @@
     [Id]         INT            IDENTITY (1, 1) NOT NULL,
     [SearchDate] DATETIME       NOT NULL,
     [UserId]     NVARCHAR (450) NOT NULL,
-    [Mushroom]   NCHAR (64)     NOT NULL,
+    [Mushroom]   NVARCHAR(64)     NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
 );
