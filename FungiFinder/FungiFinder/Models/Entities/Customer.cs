a7ac2920ee6ab577671fc3e981c27e6eca0e1ffa
Jens Lövgren <jens.lovgren@outlook.com>
2020-01-23 20:07:32 +0100

    Scaffolding EF to get the new prop "Rating", added Rating to the viewmodel and view för library result

---
+++ FungiFinder/FungiFinder/Models/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FungiFinder.Models.Entities
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
