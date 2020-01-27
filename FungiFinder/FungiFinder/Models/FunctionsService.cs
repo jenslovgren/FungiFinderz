7bb52fe92a7d6bf42c6a78e6ed5e84c2663a29f3
Eric <eric.runer@gmail.com>
2020-01-27 16:37:25 +0100

    Trying to load position from DB to google maps

--- FungiFinder/FungiFinder/Models/FunctionsService.cs
+++ FungiFinder/FungiFinder/Models/FunctionsService.cs
@@ -82,6 +82,27 @@ namespace FungiFinder.Models
             return resultList.ToArray();
         }
 
+        internal async Task<AccountProfileVM> GetUserLocations()
+        {
+            var user = await userManager.GetUserAsync(accessor.HttpContext.User);
+
+            AccountProfileVM vm = new AccountProfileVM();
+
+
+
+
+            vm.MapLocation = context.MapLocation
+                .Where(o => o.UserId == userManager
+                .GetUserId(accessor.HttpContext.User))
+                .Select(o => new FunctionMapVM { LocationName = o.LocationName, Latitude = o.Latitude, Longitude = o.Longitude })
+                //.OrderBy(o => o.SearchDate)
+                .Take(5)
+                .ToArray();
+
+            return vm;
+
+        }
+
         internal async Task SaveLocation(FunctionMapVM vm)
         {
            
@@ -91,14 +112,7 @@ namespace FungiFinder.Models
             context.SaveChanges();
 
 
-            //Skall nog flyttas... 
-            //profil.Locations = context.Locations
-            //      .Where(o => o.UserId == userManager
-            //      .GetUserId(accessor.HttpContext.User))
-            //      .Select(o => new FunctionMapVM { LocationName = o.LocationName, Latitude = o.Latitude, Longitude = o.Longitude })
-            //      .OrderBy(o => o.SearchDate)
-            //      .Take(5)
-            //      .ToArray();
+            
 
 
 
