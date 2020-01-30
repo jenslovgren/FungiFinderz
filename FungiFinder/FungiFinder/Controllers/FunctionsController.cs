225850b756c56e95965062b2d868d9f01fe5dbaa
PontusNelander <pontus.nelander@live.se>
2020-01-30 08:56:53 +0100

    Moved ChangeFileSignature method to the service.

--- FungiFinder/FungiFinder/Controllers/FunctionsController.cs
+++ FungiFinder/FungiFinder/Controllers/FunctionsController.cs
@@ -51,8 +51,9 @@ namespace FungiFinder.Controllers
         [HttpPost]
         public async Task<IActionResult> FileUpload(IFormFile file)
         {
-            if (!Utils.CheckFileSignature(file.OpenReadStream()))
+            if (!service.CheckFileSignature(file.OpenReadStream()))
                 return BadRequest("error");
+
             if (file?.Length > 0)
             {
                 var filePath = Path.Combine(hostEnvironment.WebRootPath, "Images/Uploads", file.FileName);
