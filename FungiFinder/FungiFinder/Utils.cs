96898d2c375cbaac1b31294007c72c0eafdde9db
Jens Lövgren <jens.lovgren@outlook.com>
2020-01-30 09:19:21 +0100

    Added Utils again

--- FungiFinder/FungiFinder/Utils.cs
+++ FungiFinder/FungiFinder/Utils.cs
@@ -9,6 +9,29 @@ namespace FungiFinder
 {
     public static class Utils
     {
-        
+        public static bool CheckFileSignature(Stream stream)
+        {
+            Dictionary<string, List<byte[]>> _fileSignature = new Dictionary<string, List<byte[]>>
+            {
+                { ".jpeg", new List<byte[]>
+                    {
+                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE2 },
+                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE3 },
+                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 },
+                        new byte[] { 0xFF, 0xD8, 0xFF, 0xE8 },
+                    }
+                },
+            };
+
+            using (var reader = new BinaryReader(stream))
+            {
+                var signatures = _fileSignature[".jpeg"];
+                var headerBytes = reader.ReadBytes(signatures.Max(m => m.Length));
+
+                return signatures.Any(signature =>
+                    headerBytes.Take(signature.Length).SequenceEqual(signature));
+            }
+        }
     }
 }
